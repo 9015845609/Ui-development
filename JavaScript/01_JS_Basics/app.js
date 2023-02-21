1cf7238c954645b4175dd4e133fa6550a665a2b1
sumit <sumit.kush0106@gmail.com>
2023-02-21 22:47:35 +0530

    JS Variable Declartion

---
+++ JavaScript/01_JS_Basics/app.js
@@ -0,0 +1,36 @@
+// log , info ,warn , error , table
+
+/* javaScript multi line comment */
+
+
+// log
+console.log('Iam a log message');
+
+// info
+console.info('Iam an information message');
+
+//warm
+console.warn('Iam a Warning Message');
+
+//error
+
+console.error('Iam an Error Message');
+
+//Table
+
+let student = {
+    name :'Rajan',
+    age :22,
+    course :'CSS'
+};
+console.table(student);
+
+
+/*chrome - V8 Engine 
+FireFox -> Mozilla -> Spider Monkey
+IE -> Microsoft -> Chakra
+
+*/
+
+
+
